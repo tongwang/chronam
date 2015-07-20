c93ab3bb3989c31a31bb495d03ff031d8d8a1b8e
john scancella <jsca@loc.gov>
2015-07-20 09:26:26 -0400

    install mysql separately

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -1,6 +1,7 @@
 #1/usr/bin/env bash
 
 aptGetDependencies=("python-dev" "python-virtualenv" "mysql-server" "libmysqlclient-dev" "apache2" "libapache2-mod-wsgi" "jetty8" "libxml2-dev" "libxslt-dev" "libjpeg-dev" "graphicsmagick")
+aptGetMysqlDependencies=("mysql-server" "libmysqlclient-dev")
 
 log "install dependencies"
 if [ `which apt-get` != "" ]
@@ -9,10 +10,8 @@ then
   log "Update packages"
   sudo apt-get update >> $LOG_FILE
   
-  #so that the install doesn't prompt us for a password
-  sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '
-  sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '
-
   log "Install ${aptGetDependencies[@]}"
   sudo apt-get install ${aptGetDependencies[@]} -y >> $LOG_FILE
+  #install the mysql dependencies separately cause they will need a root password
+  sudo apt-get install ${aptGetMysqlDependencies[@]} -y 
 fi

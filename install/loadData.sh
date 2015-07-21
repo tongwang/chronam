da1d6c90e01f64a7993aae5b50d8cd691801d7b5
john scancella <jsca@loc.gov>
2015-07-21 16:53:26 -0400

    modifying settings.py before loading data so that command behaves correctly

--- install/loadData.sh
+++ install/loadData.sh
@@ -5,4 +5,6 @@ LOG_FILE=install.log
 cd ${chronamDir}/data
 wget --recursive --no-host-directories --cut-dirs 1 --reject index.html* --include-directories /data/batches/batch_uuml_thys_ver01/ http://chroniclingamerica.loc.gov/data/batches/batch_uuml_thys_ver01/
 
+sed -i -E "s#/opt/chronam#${chronamDir}# ${chronamDir}/settings.py
+
 django-admin.py load_batch ${chronamDir}/data/batches/batch_uuml_thys_ver01

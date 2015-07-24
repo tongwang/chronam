6450eece8fd6d273fa9abc56ae5092d61780304c
john scancella <jsca@loc.gov>
2015-07-24 14:42:35 -0400

    removing unrecognized --migrate flag from db sync

--- install/django.sh
+++ install/django.sh
@@ -3,6 +3,6 @@
 export DJANGO_SETTINGS_MODULE=chronam.settings
 
 log "setup chronam as a django application"
-django-admin.py syncdb --noinput --migrate >> $LOG_FILE
+django-admin.py syncdb --noinput >> $LOG_FILE
 django-admin.py chronam_sync --skip-essays >> $LOG_FILE
 django-admin.py collectstatic --noinput >> $LOG_FILE

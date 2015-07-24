30a7ac388203dfd0e7ab84c1bc76b747535ba69e
john scancella <jsca@loc.gov>
2015-07-24 12:38:09 -0400

    removed south since it is deprecated

--- install/django.sh
+++ install/django.sh
@@ -3,6 +3,7 @@
 export DJANGO_SETTINGS_MODULE=chronam.settings
 
 log "setup chronam as a django application"
-django-admin.py syncdb --noinput --migrate >> $LOG_FILE
+django-admin.py syncdb makemigrations  >> $LOG_FILE
+django-admin.py syncdb migrate >> $LOG_FILE
 django-admin.py chronam_sync --skip-essays >> $LOG_FILE
 django-admin.py collectstatic --noinput >> $LOG_FILE

8ccdd2bb8324c75b5f6c892597af27cdc507ed7c
john scancella <jsca@loc.gov>
2015-07-24 14:14:12 -0400

    quieted wget data downloads

--- install/loadData.sh
+++ install/loadData.sh
@@ -4,7 +4,7 @@ LOG_FILE=install.log
 
 log "download batch_uuml_thys_ver01 for use as test data"
 cd ${chronamDir}/data
-wget --recursive --no-host-directories --cut-dirs 1 --reject index.html* --include-directories /data/batches/batch_uuml_thys_ver01/ http://chroniclingamerica.loc.gov/data/batches/batch_uuml_thys_ver01/ >> $LOG_FILE
+wget --quiet --recursive --no-host-directories --cut-dirs 1 --reject index.html* --include-directories /data/batches/batch_uuml_thys_ver01/ http://chroniclingamerica.loc.gov/data/batches/batch_uuml_thys_ver01/
 
 sed -i -E "s#/opt/chronam#${chronamDir}#" ${chronamDir}/settings.py
 
